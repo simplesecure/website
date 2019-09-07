e99adcf88fa9e9e596c88f9d886960e02ce89fad
Alex Carreira <aycarrei@gmail.com>
2019-09-07 09:41:47 -0700

    Fixes for release of new web page: change names to silver, gold, platinum. Change linking to inline pages for runkit, demo. Add blog button. Introduce Mailchip popout.

--- forms/mailchimp.php
+++ forms/mailchimp.php
@@ -4,19 +4,19 @@
     // Log in to MailChimp and create an API key under:
     // [ Account ] -> [ Extras ] -> [ API Keys ]
     $apiKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-us8';
-    
+
     // Find your list ID by opening the list in MailChimp, then:
     // [ Settings ] -> [ List name and defaults ]
-    $listId = 'abcdefghij';
-    
+    $listId = 'ef17d43d57';
+
     // Form data to use as email field
     // (The name="..." value from the email field in your HTML form)
     $emailField = 'email';
 
     // Fields to be submitted to your MailChimp list along with the email address
-    // In this example, "NAME" is the field in your MailChimp list, 
+    // In this example, "NAME" is the field in your MailChimp list,
     // and $_POST["name"] is the form data from the user to fill that field
-    $mergeFields = array( 
+    $mergeFields = array(
       "NAME" => $_POST["name"],
     );
 
@@ -29,13 +29,13 @@
 
     // Text to show user upon successful subscribe operation
     $successMessage = "Thanks for subscribing, please check your inbox for confirmation.";
-    
+
     // Text to show when the user is already subscribed to the list
     $alreadySubscribed = "You are already subscribed to this list.";
 
     // Text to show when the user is already subscribed to the list
     $checkConfirmation = "Your subscription is pending, check your inbox for a confirmation link.";
-    
+
     // Google reCAPTCHA
     // If your form is configured with a reCAPTCHA widget, this secret key will be used to validate with Google's server.
     $recaptchaSecretKey        = 'insert-your-recaptcha-secret-key-here';
